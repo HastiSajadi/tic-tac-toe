307c73a5d76658db5ef8ecc8740e5caacaed3126
hasti sajadi <hastiisajadii@gmail.com>
2023-07-09 18:00:45 +0430

    winning condition

--- script.js
+++ script.js
@@ -32,4 +32,32 @@ function addGo(e){
     }
     infoDisplay.textContent="It is now "+go+"'s turn";
     e.target.removeEventListener('click', addGo);
+    checkScores();
+}
+
+function checkScores(){
+    const allCells = document.querySelectorAll('.cell');
+    const winningCombos=[
+        [0,1,2], [3,4,5],[6,7,8],
+        [0,3,6], [1,4,7],[2,5,8],
+        [2,4,6],[0,4,8]
+    ]
+    winningCombos.forEach(array =>{
+        const circleWins= array.every(cell =>
+            allCells[cell].firstChild?.classList.contains('circle'))
+
+        if (circleWins){
+            infoDisplay.textContent = "Circle Wins!"
+            allCells.forEach(cell => cell.removeEventListener('click' , addGo));
+        }    
+    })
+    winningCombos.forEach(array =>{
+        const crossWins= array.every(cell =>
+            allCells[cell].firstChild?.classList.contains('cross'))
+
+        if (crossWins){
+            infoDisplay.textContent = "cross Wins!"
+            allCells.forEach(cell => cell.removeEventListener('click' , addGo));
+        }    
+    })
 }
