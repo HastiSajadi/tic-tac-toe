51a56cde7ea41b97d23564f44b81b4615b0db085
hasti sajadi <hastiisajadii@gmail.com>
2023-07-09 17:34:18 +0430

    add e event function for create a cirle and cross in the cell

--- script.js
+++ script.js
@@ -1,13 +1,34 @@
 const gameBoard = document.getElementById('gameboard');
 const infoDisplay = document.getElementById('info');
+infoDisplay.textContent="Circle goes first!";
+
+let go = "circle";
 
 let boardCell = [
     "", "", "",  "", "", "",  "", "", "", 
 ]
 
-boardCell.forEach((cell , index)=> {
-    const createCell = document.createElement('div');
-    createCell.classList.add('cell');
-    createCell.id = index;
-    gameBoard.append(createCell);
-})
+
+function createBoard(){
+    boardCell.forEach((cell , index)=> {
+        const createCell = document.createElement('div');
+        createCell.classList.add('cell');
+        createCell.id = index;
+        createCell.addEventListener('click' , addGo());
+        gameBoard.append(createCell);
+    })
+}
+createBoard();
+
+function addGo(e){
+    const goDisplay = document.createElement('div');
+    goDisplay.classList.add(go);
+    e.target.append(goDisplay);
+
+    if(go === "circle"){
+        go = "cross"
+    }else{
+        go = "circle"
+    }
+    
+}
